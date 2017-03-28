33d78eb4d9f19cadca8aa569e0905f895de82136
Oskar Berg <oskar.berg@digpro.se>
2017-03-28 09:56:19 +0200

    Changed to OL4

--- projects/2D/app.js
+++ projects/2D/app.js
@@ -7,3 +7,17 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+
+
+  var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([37.41, 8.82]),
+      zoom: 10
+    })
+  });
