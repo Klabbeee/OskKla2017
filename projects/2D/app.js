d4bd0b3ce22d89b60f458b0ddc6918bc281790e3
Klas Gustafsson <klas.gustafsson@digpro.se>
2017-04-03 11:19:32 +0200

    Lagt till geopositionering

--- projects/2D/app.js
+++ projects/2D/app.js
@@ -6,6 +6,11 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+var view = new ol.View({
+  center: ol.proj.fromLonLat([18.063240, 59.334591]),
+  zoom: 10
+  });
+
 
   var map = new ol.Map({
     target: 'map',
@@ -14,8 +19,66 @@ function closeNav() {
         source: new ol.source.OSM()
       })
     ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([18.063240, 59.334591]),
-      zoom: 10
-    })
+    view: view
   });
+
+
+
+var geolocation = new ol.Geolocation({
+  projection: view.getProjection()
+});
+
+// function el(id) {
+//   return document.getElementById(id);
+// }
+
+geolocation.setTracking(true);
+
+
+// update the HTML page when the position changes.
+// geolocation.on('change', function() {
+//   el('accuracy').innerText = geolocation.getAccuracy() + ' [m]';
+//   el('altitude').innerText = geolocation.getAltitude() + ' [m]';
+//   el('altitudeAccuracy').innerText = geolocation.getAltitudeAccuracy() + ' [m]';
+//   el('heading').innerText = geolocation.getHeading() + ' [rad]';
+//   el('speed').innerText = geolocation.getSpeed() + ' [m/s]';
+// });
+
+// handle geolocation error.
+geolocation.on('error', function(error) {
+  var info = document.getElementById('info');
+  info.innerHTML = error.message;
+  info.style.display = '';
+});
+
+var accuracyFeature = new ol.Feature();
+geolocation.on('change:accuracyGeometry', function() {
+  accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+});
+
+var positionFeature = new ol.Feature();
+positionFeature.setStyle(new ol.style.Style({
+  image: new ol.style.Circle({
+    radius: 6,
+    fill: new ol.style.Fill({
+      color: '#3399CC'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#fff',
+      width: 2
+    })
+  })
+}));
+
+geolocation.on('change:position', function() {
+  var coordinates = geolocation.getPosition();
+  positionFeature.setGeometry(coordinates ?
+      new ol.geom.Point(coordinates) : null);
+});
+
+new ol.layer.Vector({
+  map: map,
+  source: new ol.source.Vector({
+    features: [accuracyFeature, positionFeature]
+  })
+});
