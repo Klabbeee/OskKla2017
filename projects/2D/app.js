5dd877026339ad4120d46538bff5ebcd0fd942a5
Klas Gustafsson <klas.gustafsson@digpro.se>
2017-04-13 14:03:39 +0200

    Data

--- projects/2D/app.js
+++ projects/2D/app.js
@@ -37,8 +37,11 @@ function closeAssign() {
 
 /*** Map ***/
 
+proj4.defs("EPSG:3006","+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
+
 var view = new ol.View({
-  center: ol.proj.fromLonLat([18.063240, 59.334591]),
+  projection: ol.proj.get('EPSG:3006'),
+  center: [670162.497556056, 6579305.28607494],
   zoom: 14
   });
 
@@ -131,8 +134,327 @@ function myLocation() {
   var coordinates = geolocation.getPosition();
   document.getElementById("bottomlabel").innerHTML = coordinates;
   view.setZoom(14);
+  console.log(waterLayers.nodeLayer.getVisible());
 };
 
-geolocation.on('change', function myLocation() {
+geolocation.on('change:toMyLocation', function myLocation() {
   map.getView().setCenter(geolocation.getPosition());
 });
+
+
+// *****CODE FROM ELLA AND HILDING******
+
+  //Makes HTTP-request to the file, via the python http server.
+  //Python http-server is started with: python -m http.server
+  function makeRequest (method, url) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = function () {
+        if (this.status >= 200 && this.status < 300) {
+          resolve(xhr.response);
+        } else {
+          reject({
+            status: this.status,
+            statusText: xhr.statusText
+          });
+        }
+      };
+      xhr.onerror = function () {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      };
+      xhr.send();
+    });
+  }
+
+
+init = function() {
+  //Requests from all the different text files.
+  //To be implemeted: somehow group them together already in the requests-phase.
+  requests = {'gasArc':
+  makeRequest('GET', '/arc/gas_arcs'),
+  'gasNode':
+  makeRequest('GET', '/node/gas_arcs_vertices_pgr'),
+  'heatArc':
+  makeRequest('GET', '/arc/heat_arcs'),
+  'heatNode':
+  makeRequest('GET', '/node/heat_nodes'),
+  'waterArc':
+  makeRequest('GET', '/arc/vatten_arc'),
+  'waterNode':
+  makeRequest('GET', '/node/vatten_arc_vertices_pgr'),
+  'heatConn':
+  makeRequest('GET', '/conn/heat_cust'),
+  'gasConn':
+  makeRequest('GET', '/conn/gas_cust'),
+  'waterConn':
+  makeRequest('GET', '/conn/vatten_cust'),
+  'heatmap': 
+  makeRequest('GET', '/heatmap'),
+  'repairs': 
+  makeRequest('GET', '/repairs'),
+  'installations': 
+  makeRequest('GET', '/installations'),
+  'customers': 
+  makeRequest('GET', '/customers')
+}
+Promise
+.props(requests)
+.then(function(responses) {
+  waterArcs = responses.waterArc
+  waterNodes = responses.waterNode
+    
+    connection = [] 
+    data = []
+    network = []
+    workLayers = []
+    
+    
+    outdata = dataMaker(waterArcs, waterNodes);
+    waterLayers = layerMaker(outdata.arcSource, outdata.nodeSource);
+
+    map.addLayer(waterLayers.nodeLayer);
+    waterLayers.nodeLayer.setVisible(false);
+    map.addLayer(waterLayers.arcLayer);
+    waterLayers.arcLayer.setVisible(false);
+      //Stores the raw data
+    //To access the water layer: waterLayers.waterLayer
+    //So put your map-stuff here somewhere
+
+  })
+.catch(function (err) {
+  console.error('Augh, there was an error!', err.statusText);
+  console.error(err);
+});
+};
+
+
+function dataMaker(json_arc, json_node){
+  var arcSource_in = new ol.source.Vector()
+  var nodeSource_in = new ol.source.Vector()
+  
+  arcSource_in.addFeatures(new ol.format.GeoJSON().readFeatures(JSON.parse(json_arc)))
+  nodeSource_in.addFeatures(new ol.format.GeoJSON().readFeatures(JSON.parse(json_node)))
+  return {'arcSource': arcSource_in, 'nodeSource':nodeSource_in}
+}
+
+function layerMaker(arcSource, nodeSource){
+  arcLayer = new ol.layer.Vector({
+    source: arcSource,
+    style: styleFunction('arc', 'water', false),
+  visible: true,
+    type: 'arc'
+  })
+
+  nodeLayer = new ol.layer.Vector({
+    source: nodeSource,
+    style: styleFunction('node', 'water', false),
+  visible:true,
+    type: 'node'
+  })
+  
+  // console.log(waterLayers.arcLayer)
+  return {'arcLayer': arcLayer, 'nodeLayer':nodeLayer}
+  }
+  
+  //StyleShit
+  
+  var gas = {
+ 'connection': new ol.style.Style({
+  image: new ol.style.Circle({
+   fill: new ol.style.Fill({
+    color: '#D500F9'
+  }),
+   radius: 5
+ })
+}),
+ 'arc': new ol.style.Style({
+  stroke: new ol.style.Stroke({
+    color: '#D500F9',
+    width: 2
+  })
+}),
+ 'node': new ol.style.Style({
+  text: new ol.style.Text({
+    text: '\uf0e7 ',
+    font: 'normal 35px FontAwesome',
+    textBaseline: 'Bottom',
+    fill: new ol.style.Fill({
+      color: '#D500F9',
+    })
+  })   
+})};
+   //Visual settings for heating
+   var heating = {
+     'connection': new ol.style.Style({
+      image: new ol.style.Circle({
+       fill: new ol.style.Fill({
+        color: 'black'
+      }),
+       radius: 5
+     })
+    }),
+
+     'arc': new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        color: 'black',
+        lineDash: [5,1,5],
+        width: 2
+      })
+    }),
+     'node': new ol.style.Style({
+       text: new ol.style.Text({
+        text: '\uf0e7',
+        font: 'normal 35px FontAwesome',
+        textBaseline: 'Bottom',
+        stroke: new ol.style.Stroke({
+          color: 'rgba(255, 140, 0, 0.6)',
+          width: 8}),
+        fill: new ol.style.Fill({
+          color: 'black',
+        })
+      })   
+     })};
+
+     var brokenHeating = {
+       'connection': new ol.style.Style({
+        image: new ol.style.Circle({
+         fill: new ol.style.Fill({
+          color: 'black'
+        }),
+         radius: 5,
+         stroke: new ol.style.Stroke({
+          color: 'rgba(255, 140, 0, 0.6)',
+          width: 10})
+       })
+      }),
+
+       'arc': new ol.style.Style({
+        stroke: new ol.style.Stroke({
+          color: 'black',
+          lineDash: [5,1,5],
+          width: 4
+        })
+      }),
+       'node': new ol.style.Style({
+         text: new ol.style.Text({
+          text: '\uf0e7',
+          font: 'normal 35px FontAwesome',
+          textBaseline: 'Bottom',
+          stroke: new ol.style.Stroke({
+            color: 'rgba(255, 140, 0, 0.6)',
+            width: 8}),
+          fill: new ol.style.Fill({
+            color: 'black',
+          })
+        })   
+       })};
+
+   //Visual settings for water
+   var water = {
+     'connection': new ol.style.Style({
+      image: new ol.style.Circle({
+       fill: new ol.style.Fill({
+        color: '#2196F3'
+      }),
+       radius: 5
+     })
+    }),
+     'arc': new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        color: '#2196F3',
+        lineDash: [5,5],
+        width: 3
+      })
+    }),
+     'node': new ol.style.Style({
+       text: new ol.style.Text({
+        text: '\uf0e7 ',
+        font: 'normal 35px FontAwesome',
+        textBaseline: 'Bottom',
+        fill: new ol.style.Fill({
+          color: '#2196F3'}),
+        stroke: new ol.style.Stroke({
+          color: 'rgba(255, 140, 0, 0.6)',
+          width: 8}),
+      })   
+     })};
+
+     var brokenWater = {
+       'connection': new ol.style.Style({
+        image: new ol.style.Circle({
+         fill: new ol.style.Fill({
+          color: '#2196F3'
+        }),
+         radius: 5,
+         stroke: new ol.style.Stroke({
+          color: 'rgba(255, 140, 0, 0.6)',
+          width: 10})
+       })
+      }),
+       'arc': new ol.style.Style({
+        stroke: new ol.style.Stroke({
+          color: '#2196F3',
+          lineDash: [8,8],
+          width: 4
+        })
+      }),
+       'node': new ol.style.Style({
+         text: new ol.style.Text({
+          text: '\uf0e7 ',
+          font: 'normal 35px FontAwesome',
+          textBaseline: 'Bottom',
+          fill: new ol.style.Fill({
+            color: '#2196F3'}),
+          stroke: new ol.style.Stroke({
+            color: 'rgba(255, 140, 0, 0.6)',
+            width: 8}),
+        })   
+       })};
+
+       var customerStyle = new ol.style.Style({
+         text: new ol.style.Text({
+          text: '\uf007',
+          font: 'normal 20px FontAwesome',
+          textBaseline: 'Bottom',
+          fill: new ol.style.Fill({
+            color: '#FFFFFF',
+          }),
+          stroke: new ol.style.Stroke({
+            color: '#000000',
+            width: 2}),
+        })   
+       })
+
+       var styles = {'heating':heating, 'gas':gas, 'water':water}
+       var brokenStyles = {'heating':brokenHeating, 'water':brokenWater}
+
+
+   //Takes the type of network and the object to visualize as input.
+   var styleFunction = function(visualize, network_type, broken) {
+    if(broken==true){
+      return brokenStyles[network_type][visualize];
+    }else{
+      return styles[network_type][visualize];
+    }
+    
+  };
+
+  function nodeOnClick()  {
+      if (waterLayers.nodeLayer.getVisible() == false){
+      waterLayers.nodeLayer.setVisible(true);
+     }else{
+      waterLayers.nodeLayer.setVisible(false);
+     }
+  };
+
+  function arcOnClick()  {
+    if (waterLayers.arcLayer.getVisible() == false){
+    waterLayers.arcLayer.setVisible(true);
+    }else{
+    waterLayers.arcLayer.setVisible(false);
+    }
+};
